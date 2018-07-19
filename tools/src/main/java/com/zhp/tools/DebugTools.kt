d2671526a259113f2f3396f655a1d6938a59ae07
pzhang <pengzh.leo@gmail.com>
2018-07-19 16:24:17 +0800

    补充工具类方法

--- tools/src/main/java/com/zhp/tools/DebugTools.kt
+++ tools/src/main/java/com/zhp/tools/DebugTools.kt
@@ -2,6 +2,8 @@ package com.zhp.tools
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import okhttp3.Interceptor
 import tech.linjiang.pandora.Pandora
 
 fun init(app: Application) {
@@ -13,6 +15,13 @@ fun init(app: Application) {
  * https://github.com/whataa/pandora
  */
 fun initPandora(app: Application) {
+    Pandora.init(app)
+}
+
+/**
+ * https://github.com/whataa/pandora
+ */
+fun initPandoraEnableShake(app: Application) {
     Pandora.init(app).enableShakeOpen()
 }
 
@@ -21,4 +30,20 @@ fun initPandora(app: Application) {
  */
 fun initStetho(app: Application) {
     Stetho.initializeWithDefaults(app)
+}
+
+fun openPandora() {
+    Pandora.get().open()
+}
+
+fun closePandora() {
+    Pandora.get().close()
+}
+
+fun getPandoraInterceptor(): Interceptor {
+    return Pandora.get().interceptor
+}
+
+fun getStethoIntercepter(): Interceptor {
+    return StethoInterceptor()
 }
